bec94aafb45d61a19d8f7871accc9fa67600c884
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-06-23 10:37:36 -0700

    New Commit

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	'secret': 'ilovescotchyscotch',
-	'database': 'MONGODB_URI'
+	'database': 'mongodb://heroku_wzwnwgvr:qkms5u5klb2g63gjsnqpd0q3p0@ds135552.mlab.com:35552/heroku_wzwnwgvr'
 
 
 };
