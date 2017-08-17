218a9f4b493a8b5de8a5641fbc61eac3c845ffe9
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-08-17 15:07:04 -0700

    Added the CORS options

--- server.js
+++ server.js
@@ -33,9 +33,16 @@ require('./config/passport/routes-passport')(passport); // pass passport for con
 	// app.use(passport.session()); // persistent login sessions
 	// app.use(flash()); // use connect-flash for flash messages stored in session
 
+	var corsOptions = {
+  "origin": "*",
+  "allowedHeaders":['Content-Type', 'Authorization'],
+	"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+	"credentials":true
+}
 	//Setting Headers ===================================================
-	app.use(cors());
-	app.options('*', cors());
+	app.use(cors(corsOptions));
+	//app.options('*', cors(corsOptions));
 
 // routes ======================================================================
 require('./features/routes')(app, passport); // load our routes and pass in our app and fully configured passport
