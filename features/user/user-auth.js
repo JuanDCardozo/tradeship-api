0ad2a6df738de7c3b3d3b7b0429b53f335168f89
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-08-19 17:17:35 -0700

    Implement local-loging and beautify code

--- features/user/user-auth.js
+++ features/user/user-auth.js
@@ -1,84 +1,100 @@
+// ==================================================================
+// user-auth.js handles all the first user authentifications
+//
+//
+// ==================================================================
+
 const jwt = require('jsonwebtoken');
 const configAuth = require('../../config/passport/auth-passport');
 
-module.exports = function(app, passport){
-// =============================================================================
-// AUTHENTICATE (FIRST LOGIN) ==================================================
-// =============================================================================
+module.exports = function(app, passport) {
+  // ==================================================================
+  // HELPER FUNCTIONS =================================================
+  // ==================================================================
 
-// Generate an Access Token for the given User ID
-function generateAccessToken(userId) {
+  // Generate an Access Token for the given User ID
+  function generateAccessToken(userId) {
     const expiresIn = '1 hour';
     const audience = configAuth.token.audience;
     const issuer = configAuth.token.issuer;
     const secret = configAuth.token.secret;
 
     const token = jwt.sign({}, secret, {
-        expiresIn: expiresIn,
-        audience: audience,
-        issuer: issuer,
-        subject: userId.toString()
+      expiresIn: expiresIn,
+      audience: audience,
+      issuer: issuer,
+      subject: userId.toString()
     });
 
     return token;
-}
+  }
 
-// Generate the Token for the user authenticated in the request
-function generateUserToken(req, res) {
-  console.log('inside generate UserToken');
+  // Generate the Token for the user auth then redirect to client
+  function generateUserTokenRedirect(req, res) {
     const accessToken = generateAccessToken(req.user._id);
+    console.log('GenerateUserTokenRedirect(), with token: ' + accessToken);
+    res.redirect('http://localhost:4200/home/?accessToken=' + accessToken);
 
-    // res.redirect(302,'http://localhost:4200/home/?accessToken=');
-		// res.writeHead(302, {
-    // //   'Access-Control-Allow-Origin':'*',
-    // //   'Access-Control-Allow-Headers':'Content-Type,Origin, X-Requested-With, Content-Type, Accept, Authorization, WWW-Authenticate, X-BLURR-DEBUG',
-    // //   'Access-Control-Request-Method':	'POST,GET,OPTIONS',
-    //             'Location': 'http://localhost:4200/home/?accessToken='+accessToken
-    //         });
-		// res.end();
+  }
 
-    // res.send({ redirect: 'http://localhost:4200/home/?accessToken='+accessToken })
-    res.send('http://localhost:4200/home/?accessToken='+accessToken);
+  // Generate the Token for the user auth then send to client
+  function generateUserTokenSend(req, res) {
+    const accessToken = generateAccessToken(req.user._id);
+    console.log('GenerateUserTokenSend(), with token: ' + accessToken);
+    res.send('http://localhost:4200/home/?accessToken=' + accessToken);
 
-}
+  }
 
-	// locally --------------------------------
-		// LOGIN ===============================
-		// show the login form
-		app.get('/login', function(req, res) {
-			res.json({'HELLO':'WORLD'});
-		});
-
-		// process the login form
-		app.post('/login', passport.authenticate('local-login', { session: false }),
-generateUserToken);
-
-		// SIGNUP =================================
-		// show the signup form
-		app.get('/signup', generateUserToken);
-
-		// process the signup form
-		app.post('/signup',
-		passport.authenticate('local-signup', { session: false }),
-generateUserToken);
-	// facebook -------------------------------
-
-		// send to facebook to do the authentication
-		app.get('/auth/facebook', passport.authenticate('facebook', {session: false, scope : 'email' }));
-
-		// handle the callback after facebook has authenticated the user
-		app.get('/auth/facebook/callback',
-		passport.authenticate('facebook', { session: false }),
-generateUserToken);
+  // ==================================================================
+  // LOCAL SIGNUP-LOGIN  ==============================================
+  // ==================================================================
 
 
-	// google ---------------------------------
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+      session: false
+    }),
+    generateUserTokenSend);
 
-		// send to google to do the authentication
-		app.get('/auth/google', passport.authenticate('google', { session: false, scope : ['profile', 'email'] }));
+  // process the signup form
+  app.post('/signup',
+    passport.authenticate('local-signup', {
+      session: false
+    }),
+    generateUserTokenSend);
 
-		// the callback after google has authenticated the user
-		app.get('/auth/google/callback',
-		passport.authenticate('google', { session: false }),
-generateUserToken);
+  // =================================================================
+  // FACEBOOK LOGIN ==================================================
+  // =================================================================
+
+  // send to facebook to do the authentication
+  app.get('/auth/facebook', passport.authenticate('facebook', {
+    session: false,
+    scope: 'email'
+  }));
+
+  // handle the callback after facebook has authenticated the user
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', {
+      session: false
+    }),
+    generateUserTokenRedirect);
+
+
+  // ================================================================
+  // GOOGLE LOGIN  ==================================================
+  // ================================================================
+
+  // send to google to do the authentication
+  app.get('/auth/google', passport.authenticate('google', {
+    session: false,
+    scope: ['profile', 'email']
+  }));
+
+  // the callback after google has authenticated the user
+  app.get('/auth/google/callback',
+    passport.authenticate('google', {
+      session: false
+    }),
+    generateUserTokenRedirect);
 }
