044ad62ddc68bdfeefe3b5b70c8b2f30d996b8ee
Juan D. Cardozo <jcardozo@ucsc.edu>
2017-08-25 16:20:50 -0700

    used jwt.verify to secure endpoints, however we need to create it as a helper function and make sure we are setting the auth header correctly on the client side

--- config/passport/jwt-passport.js
+++ config/passport/jwt-passport.js
@@ -1,25 +1,46 @@
-const passport = require('passport');
-const passportJwt = require('passport-jwt');
+//const passport = require('passport');
+//const passportJwt = require('passport-jwt');
 
-module.exports = function(passport, User, configAuth){
+// module.exports = function(passport, User, configAuth) {
+//   var jwtOptions = {}
+//   jwtOptions.jwtFromRequest = passportJwt.ExtractJwt.fromHeader("authorization");
+//   jwtOptions.secretOrKey = configAuth.token.secret;
+//
+//   passport.use(new passportJwt.Strategy(jwtOptions, function(jwt_payload, done) {
+//     console.log('IN PASSPORT-JWT!!!');
+//     User.findOne({
+//       '_id': jwt_payload.sub
+//     }, function(err, user) {
+//       if (err) {
+//         return done(err, false);
+//       }
+//       if (user) {
+//         done(null, user);
+//       } else {
+//         done(null, false);
+//         // or you could create a new account
+//       }
+//     });
+//   }));
+// }
+var JwtBearerStrategy = require('passport-http-bearer')
+module.exports = function(passport, User, configAuth) {
 
-passport.use(new passportJwt.Strategy( {
-    jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeader(),
-    secretOrKey: configAuth.token.secret,
-    issuer: configAuth.token.issuer,
-    audience: configAuth.token.audience
-},function(jwt_payload, done) {
-    console.log('IN PASSPORT-JWT!!!');
-    User.findOne({'facebook.id': jwt_payload.sub}, function(err, user) {
+
+  passport.use(new JwtBearerStrategy(
+    configAuth.token.secret,
+    function(token, done) {
+      console.log("Inside jwt bearer passport")
+      User.findById(token.sub, function(err, user) {
         if (err) {
-            return done(err, false);
+          return done(err);
         }
-        if (user) {
-            done(null, user);
-        } else {
-            done(null, false);
-            // or you could create a new account
+        if (!user) {
+          return done(null, false);
         }
-    });
-}));
+        return done(null, user, token);
+      });
+    }
+  ));
+
 }
--- features/user/user-data.js
+++ features/user/user-data.js
@@ -3,31 +3,49 @@
 // if the credentials are in the database then return jwt token. If
 // it is not it creates a new user and returns a token.
 // ==================================================================
-
+var configAuth = require('../../config/passport/auth-passport');
+// load up the user model
+var User = require('./user-model');
+const jwt = require('jsonwebtoken');
 module.exports = function(app, passport) {
 
-    // Returns the users profile data like name, last name, reviews, etc
-    // app.get('/profile', passport.authenticate('jwt', {
-    //     session: false
-    //   }),
-    //   function(req, res) {
-    //     console.log(req.user)
-    //     res.send(req.user);
-    //   }
-    // );
+  // Returns the users profile data like name, last name, reviews, etc
+  // app.get('/profile', passport.authenticate('jwt', {
+  //     session: false
+  //   }),
+  //   function(req, res) {
+  //     console.log(req.user)
+  //     res.send(req.user);
+  //   }
+  // );
 
-    app.get('/profile', passport.authenticate('jwt', {
-        session: false
-      }),
-      function(req, res) {
-        console.log(req.user)
-        res.jsonp({
-          data: req.user
-        });
-      );
-
-      app.post('/profile-update', function(req, res) {
-        res.send('/profile/update is not yet implemented');
-      });
-
+  app.get('/profile', function(req, res) {
+    console.log("in profile before verify")
+    console.log(req.get('Authorization'))
+    try {
+      var decoded = jwt.verify(req.get('Authorization'), configAuth.token.secret);
+    } catch (err) {
+      console.log("Error In verify")
     }
+
+    User.findById(decoded.sub, function(err, user) {
+      if (err) {
+        console.log(error)
+      }
+      if (!user) {
+        console.log("No user")
+      }
+      console.log(user)
+      res.send(user)
+    });
+    // console.log(req.user)
+    // res.jsonp({
+    //   data: req.user
+    // });
+  });
+
+  app.post('/profile-update', function(req, res) {
+    res.send('/profile/update is not yet implemented');
+  });
+
+}
